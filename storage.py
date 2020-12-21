2f294ccb2f7c5c668a94deb7c0f95d76cf90b94e
westerholz <github@westerholz.me>
2020-12-21 17:38:21 +0100

    Update and rename database.py to storage.py

---
+++ storage.py
@@ -0,0 +1,25 @@
+import configparser
+import MySQLdb.cursors
+
+insertSQL = "INSERT INTO feuchtigkeit( humidity,temperature) VALUES (%s,%s)"
+
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+
+connection = MySQLdb.connect(host = config['mysqlDB']['host'],
+                    user = config['mysqlDB']['user'],
+                    passwd = config['mysqlDB']['pass'],
+                    db = config['mysqlDB']['db'])
+cursor = connection.getCursor()
+
+def read():
+    humidity, temperature = Adafruit_DHT.read_retry(config['sensor']['type'], config['sensor']['pin'])
+    return ( humidity, temperature = Adafruit_DHT )
+
+def writeToDB(humidity, temperature):
+    value = humidity, temperature
+    cursor.execute(insertSQL, value)
+    connection.commit()
+    
+    
+       
